be9ff5659bd71c41cf4737ca33765847fbae5832
kacpermigala <kacper@migala.pl>
2023-05-17 13:35:29 +0200

    test: :white_check_mark: fix specs for category page

--- src/components/store/Cart.tsx
+++ src/components/store/Cart.tsx
@@ -1,112 +1,111 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type Product = {
-	_id: number;
-	name: string;
-	shortName: string;
-	price: number;
-	description: string;
-	feature1: string;
-	feature2: string;
-	imageDesktop: string;
-	imageMobile: string;
-	imageCart: string;
-	category: {
-		name: string;
-	};
+  _id: number;
+  name: string;
+  shortName: string;
+  price: number;
+  description: string;
+  feature1: string;
+  feature2: string;
+  imageDesktop: string;
+  imageMobile: string;
+  imageCart: string;
+  category: {
+    name: string;
+  };
 };
 
 type ProductState = Product & {
-	quantity: number;
+  quantity: number;
 };
 
 type State = {
-	products: ProductState[];
+  products: ProductState[];
 };
 
 const initialState: State = {
-	products: JSON.parse(sessionStorage.getItem('cartItems') || '[]'),
+  products: JSON.parse(sessionStorage.getItem("cartItems") || "[]"),
 };
 
 const cart = createSlice({
-	name: 'cm',
-	initialState,
-	reducers: {
-		addItem: (
-			state: State,
-			action: PayloadAction<{ quantity: number; product: Product }>
-		) => {
-			const { quantity, product } = action.payload;
-			const existingProduct = state.products.find(
-				(item) => item._id === product._id
-			);
-			if (!existingProduct) {
-				state = {
-					...state,
-					products: [...state.products, { ...product, quantity }],
-				};
-			} else {
-				state = {
-					...state,
-					products: [
-						...state.products.filter((item) => item._id !== product._id),
-						{ ...existingProduct, quantity },
-					],
-				};
-			}
-			console.log(state);
-			sessionStorage.setItem('cartItems', JSON.stringify(state.products));
-			return state;
-		},
-		addItemToCart: (state: State, action: PayloadAction<number>) => {
-			const itemId = action.payload;
-			const cartItemIndex: number = state.products.findIndex(
-				(item) => item._id === itemId
-			);
-			const cartItem = state.products[cartItemIndex];
-			if (!cartItem) {
-				return state;
-			}
-			const newState = JSON.parse(JSON.stringify(state)) as State;
-			newState.products[cartItemIndex] = {
-				...cartItem,
-				quantity: cartItem.quantity + 1,
-			};
+  name: "cm",
+  initialState,
+  reducers: {
+    addItem: (
+      state: State,
+      action: PayloadAction<{ quantity: number; product: Product }>
+    ) => {
+      const { quantity, product } = action.payload;
+      const existingProduct = state.products.find(
+        (item) => item._id === product._id
+      );
+      if (!existingProduct) {
+        state = {
+          ...state,
+          products: [...state.products, { ...product, quantity }],
+        };
+      } else {
+        state = {
+          ...state,
+          products: [
+            ...state.products.filter((item) => item._id !== product._id),
+            { ...existingProduct, quantity },
+          ],
+        };
+      }
+      sessionStorage.setItem("cartItems", JSON.stringify(state.products));
+      return state;
+    },
+    addItemToCart: (state: State, action: PayloadAction<number>) => {
+      const itemId = action.payload;
+      const cartItemIndex: number = state.products.findIndex(
+        (item) => item._id === itemId
+      );
+      const cartItem = state.products[cartItemIndex];
+      if (!cartItem) {
+        return state;
+      }
+      const newState = JSON.parse(JSON.stringify(state)) as State;
+      newState.products[cartItemIndex] = {
+        ...cartItem,
+        quantity: cartItem.quantity + 1,
+      };
 
-			return newState;
-		},
-		removeItem: (state: State, action: PayloadAction<number>) => {
-			const itemId = action.payload;
+      return newState;
+    },
+    removeItem: (state: State, action: PayloadAction<number>) => {
+      const itemId = action.payload;
 
-			const cartItemIndex: number = state.products.findIndex(
-				(item) => item._id === itemId
-			);
-			const cartItem = state.products[cartItemIndex];
-			if (!cartItem) {
-				return state;
-			}
+      const cartItemIndex: number = state.products.findIndex(
+        (item) => item._id === itemId
+      );
+      const cartItem = state.products[cartItemIndex];
+      if (!cartItem) {
+        return state;
+      }
 
-			const newState = JSON.parse(JSON.stringify(state)) as State;
+      const newState = JSON.parse(JSON.stringify(state)) as State;
 
-			if (cartItem.quantity === 1)
-				newState.products = newState.products.filter(
-					(product) => product._id !== cartItem._id
-				);
-			else
-				newState.products[cartItemIndex] = {
-					...cartItem,
-					quantity: cartItem.quantity - 1,
-				};
+      if (cartItem.quantity === 1)
+        newState.products = newState.products.filter(
+          (product) => product._id !== cartItem._id
+        );
+      else
+        newState.products[cartItemIndex] = {
+          ...cartItem,
+          quantity: cartItem.quantity - 1,
+        };
 
-			return newState;
-		},
-		removeAll: (state: State) => {
-			return {
-				...state,
-				products: [],
-			};
-		},
-	},
+      return newState;
+    },
+    removeAll: (state: State) => {
+      return {
+        ...state,
+        products: [],
+      };
+    },
+  },
 });
 
 export const cartActions = cart.actions;
--- src/tests/CategoryPage.test.jsx
+++ src/tests/CategoryPage.test.jsx
@@ -1,55 +1,54 @@
-import { screen } from '@testing-library/react';
-import { renderWithProviders } from '../utils/test-utils';
-import { prettyDOM } from '@testing-library/react';
-import { CategoryPage } from '../components/Category/CategoryPage';
-import Router from 'react-router';
+jest.mock("../client");
+import { screen, act } from "@testing-library/react";
+import { renderWithProviders } from "../utils/test-utils";
+import { CategoryPage } from "../components/Category/CategoryPage";
+import { MemoryRouter, Route, Routes } from "react-router";
+import sanityClient from "../client";
 
-jest.mock('react-router', () => ({
-	...jest.requireActual('react-router'),
-	useParams: jest.fn(),
-}));
+describe("Category Page tests", () => {
+  describe("If category doesn't exist", () => {
+    it("should show error page", () => {
+      renderWithProviders(<CategoryPage categoryid={1909} />);
 
-describe('Category Page tests', () => {
-	describe("If category doesn't exist", () => {
-		it('should show error page', () => {
-			renderWithProviders(<CategoryPage categoryid={1909} />);
+      const element = screen.getByText("Mordo, nie mam takiego produktu");
 
-			const element = screen.getByText('Mordo, nie mam takiego produktu');
+      expect(element).toBeInTheDocument();
+    });
+  });
+  describe("If category does exist", () => {
+    it("should show correct products number", async () => {
+      sanityClient.fetch.mockResolvedValue({
+        name: "Example category",
+        categories: [
+          {
+            name: "Example product",
+            description: "Product",
+            router: "/product/1",
+            imageDesktop: "url",
+            imageTablet: "url2",
+            imageMobile: "url2",
+          },
+        ],
+      });
 
-			expect(element).toBeInTheDocument();
-		});
-	});
-	describe('If category does exist', () => {
-		const sanityMock = jest.mock('@sanity/client', () => {
-			return function sanity() {
-				return {
-					fetch: () =>
-						jest.fn().mockResolvedValue({
-							name: 'Example category',
-							categories: [
-								{
-									name: 'Example product',
-									description: 'Product',
-									router: '/product/1',
-									imageDesktop: 'url',
-									imageTablet: 'url2',
-									imageMobile: 'url2',
-								},
-							],
-						}),
-				};
-			};
-		});
+      const {container} = await act(async () =>
+        renderWithProviders(
+          <MemoryRouter initialEntries={["/321312"]}>
+            <Routes>
+              <Route path="/:categoryid" element={<CategoryPage />} />
+            </Routes>
+          </MemoryRouter>
+        )
+      );
 
-		it('should show correct products number', () => {
-			jest.spyOn(Router, 'useParams').mockReturnValue({ categoryid: '1' });
-			const { container } = renderWithProviders(
-				<CategoryPage categoryid={1909} />
-			);
-			console.log(prettyDOM(container));
-			const elements = container.getElementsByClassName('lg:my-8');
-			const elementsLength = elements.length;
-			expect(elementsLength).toBe(1);
-		});
-	});
+      expect(sanityClient.fetch).toHaveBeenCalledTimes(1)
+      const elements = container.getElementsByClassName("lg:my-8");
+      const elementsLength = elements.length;
+      expect(elementsLength).toBe(1);
+    });
+  });
+
+	//check for loading
+	//if redirects work
+	//if sanity responses with error
 });
