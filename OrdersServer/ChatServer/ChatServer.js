bd1e836a5a205241b7d97e319f0de2d065dd3ba4
Oskar Meyer <oskarmeyer47@gmail.com>
2023-11-25 15:13:49 +0100

    changed few lines of code in chat server

--- OrdersServer/ChatServer/ChatServer.js
+++ OrdersServer/ChatServer/ChatServer.js
@@ -1,120 +1,127 @@
-import express from 'express' 
-import http from 'http';
-import cors from 'cors';
-import { Server } from "socket.io";
-const app = express();
-app.use(cors());
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io'
+const app = express()
+app.use(cors())
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-const io = new Server(server, {
-	cors: {
-		origin: 'https://full-backend-audio.onrender.com',
-		methods: ['GET', 'POST'],
-	},
-});
+// const io = new Server(server, {
+// 	cors: {
+// 		origin: 'https://full-backend-audio.onrender.com',
+// 		methods: ['GET', 'POST'],
+// 	},
+// });
 
-const chats = [];
+const options = {
+  cors: true,
+  origins: ['https://full-backend-audio.onrender.com'],
+  'force new connection': true,
+}
+const io = new Server(server, { options })
+
+const chats = []
 const admin = {
-	adminUserName: 'oskar',
-	adminPassword: '123',
-};
+  adminUserName: 'oskar',
+  adminPassword: '123',
+}
 
-let adminUniqueId = '';
+let adminUniqueId = ''
 
 io.on('connection', (socket) => {
-	console.log(`User connected: ${socket.id}`);
+  console.log(`User connected: ${socket.id}`)
 
-	socket.on('get_all_chats', (token) => {
-		if (token === adminUniqueId) {
-			const activeChats = chats.filter((chat) => !chat.archived);
-			socket.emit('all_chats', activeChats);
-		}
-	});
+  socket.on('get_all_chats', (token) => {
+    if (token === adminUniqueId) {
+      const activeChats = chats.filter((chat) => !chat.archived)
+      socket.emit('all_chats', activeChats)
+    }
+  })
 
-	socket.on('join_room', (roomName, productName, productPhoto) => {
-		console.log(`${roomName} it is a user data`);
-		const chat = chats.find((el) => el.roomName === roomName);
-		if (!chat) {
-			chats.push({
-				roomName,
-				productName,
-				productPhoto,
-				userId: socket.id,
-				archived: false,
-				messages: [],
-			});
-			socket.broadcast.emit('all_chats', Array.from(chats));
-		}
+  socket.on('join_room', (roomName, productName, productPhoto) => {
+    console.log(`${roomName} it is a user data`)
+    const chat = chats.find((el) => el.roomName === roomName)
+    if (!chat) {
+      chats.push({
+        roomName,
+        productName,
+        productPhoto,
+        userId: socket.id,
+        archived: false,
+        messages: [],
+      })
+      socket.broadcast.emit('all_chats', Array.from(chats))
+    }
 
-		socket.join(roomName);
-		console.log(`User with ID: ${socket.id} joined room ${roomName}`);
-	});
+    socket.join(roomName)
+    console.log(`User with ID: ${socket.id} joined room ${roomName}`)
+  })
 
-	socket.on('send_message', (data) => {
-		const chat = chats.find((chat) => chat.roomName === data.room);
-		if (chat) {
-			chat.messages.push({
-				room: data.room,
-				author: data.author,
-				message: data.message,
-				time: data.time,
-			});
-		}
-		socket.to(data.room).emit('receive_message', data);
-	});
+  socket.on('send_message', (data) => {
+    const chat = chats.find((chat) => chat.roomName === data.room)
+    if (chat) {
+      chat.messages.push({
+        room: data.room,
+        author: data.author,
+        message: data.message,
+        time: data.time,
+      })
+    }
+    socket.to(data.room).emit('receive_message', data)
+  })
 
-	socket.on('login', (userName, password, callback) => {
-		if (userName === admin.adminUserName && password === admin.adminPassword) {
-			adminUniqueId = new Date().getTime().toString(16);
-			callback(adminUniqueId);
-		} else {
-			callback(false);
-		}
-	});
+  socket.on('login', (userName, password, callback) => {
+    if (userName === admin.adminUserName && password === admin.adminPassword) {
+      adminUniqueId = new Date().getTime().toString(16)
+      callback(adminUniqueId)
+    } else {
+      callback(false)
+    }
+  })
 
-	socket.on('logout', (callback) => {
-		adminUniqueId = null;
-		callback(true);
-	});
+  socket.on('logout', (callback) => {
+    adminUniqueId = null
+    callback(true)
+  })
 
-	socket.on('leave_room', (roomName) => {
-		console.log(`${roomName} to dane użytkownika`);
-		console.log(`User with ID: ${socket.id} leaved room ${roomName}`);
-		socket.leave(roomName);
-	});
+  socket.on('leave_room', (roomName) => {
+    console.log(`${roomName} to dane użytkownika`)
+    console.log(`User with ID: ${socket.id} leaved room ${roomName}`)
+    socket.leave(roomName)
+  })
 
-	socket.on('delete_chat', (roomName, token) => {
-		if (token === adminUniqueId) {
-			const chat = chats.find((chat) => chat.roomName === roomName);
-			chat.archived = true;
-			const activeChats = chats.filter((chat) => !chat.archived);
-			socket.emit('all_chats', activeChats);
-			const archivedChats = chats.filter((chat) => chat.archived);
-			socket.emit('archived_chats', archivedChats);
-		}
-	});
+  socket.on('delete_chat', (roomName, token) => {
+    if (token === adminUniqueId) {
+      const chat = chats.find((chat) => chat.roomName === roomName)
+      chat.archived = true
+      const activeChats = chats.filter((chat) => !chat.archived)
+      socket.emit('all_chats', activeChats)
+      const archivedChats = chats.filter((chat) => chat.archived)
+      socket.emit('archived_chats', archivedChats)
+    }
+  })
 
-	socket.on('get_archived_chats', (token) => {
-		if (token === adminUniqueId) {
-			const archivedChats = chats.filter((chat) => chat.archived);
-			socket.emit('archived_chats', archivedChats);
-		}
-	});
+  socket.on('get_archived_chats', (token) => {
+    if (token === adminUniqueId) {
+      const archivedChats = chats.filter((chat) => chat.archived)
+      socket.emit('archived_chats', archivedChats)
+    }
+  })
 
-	socket.on('get_historical_messages', (roomName, callback) => {
-		const selectedChat = chats.find((chat) => chat.roomName === roomName);
-		if (selectedChat) {
-			const messages = selectedChat.messages;
-			callback(messages);
-		} else {
-			callback([]);
-		}
-	});
-});
+  socket.on('get_historical_messages', (roomName, callback) => {
+    const selectedChat = chats.find((chat) => chat.roomName === roomName)
+    if (selectedChat) {
+      const messages = selectedChat.messages
+      callback(messages)
+    } else {
+      callback([])
+    }
+  })
+})
 
 //START THE SERVER
-const port = 4000;
+const port = 4000
 server.listen(port, () => {
-	console.log(`Chat message server is running on port ${port}`);
-});
+  console.log(`Chat message server is running on port ${port}`)
+})
