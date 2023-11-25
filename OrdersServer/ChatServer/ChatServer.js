42384e9ecb5d7051a457ea3a646eb1505a11fa8b
Oskar Meyer <oskarmeyer47@gmail.com>
2023-11-25 14:49:22 +0100

    changed chat adress

--- OrdersServer/ChatServer/ChatServer.js
+++ OrdersServer/ChatServer/ChatServer.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
 	cors: {
-		origin: 'https://full-backend-audio.onrender.com:4000',
+		origin: 'https://theaudioonline.store/',
 		methods: ['GET', 'POST'],
 	},
 });
