f13cead8af8202d845bf801765e86224d76441c1
Oskar Meyer <oskarmeyer47@gmail.com>
2023-11-27 19:11:24 +0100

    changed all paths to new server

--- src/store/adminThunks.ts
+++ src/store/adminThunks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { authaActions } from './adminAuth';
 import io from 'socket.io-client';
 
-const socket = io('https://full-backend-audio.onrender.com:4000', {
+const socket = io('https://chatserver-backend.onrender.com', {
 	autoConnect: false,
 });
 
