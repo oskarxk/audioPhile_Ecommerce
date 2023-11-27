2204519160cdbdcd62073fe07f48c0f719cd1960
Oskar Meyer <oskarmeyer47@gmail.com>
2023-11-27 18:19:55 +0100

    added additional port

--- OrdersServer/ChatServer/ChatServer.js
+++ OrdersServer/ChatServer/ChatServer.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
 	cors: {
-		origin: 'https://full-backend-audio.onrender.com:4000',
+		origin: 'https://full-backend-audio.onrender.com',
 		methods: ['GET', 'POST'],
 	},
 });
